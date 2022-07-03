10db8cb047f9a8f5db765f5ab1a2014790b1cf36
Zobaer Ahmed Zihad <zobaerahmedzihad02@gmail.com>
2022-07-03 12:24:04 +0600

    connect server to mongodb database

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 5000
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+require("dotenv").config();
 
 // middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res)=>{
-    res.send("ignite visibility backend server is ready.")
-})
+// Database Connection
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0.w5uoe.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log("Database connected.");
+  client.close();
+});
 
-app.listen(port , ()=> {
-    console.log('Listening from port', port);
-})
+app.get("/", (req, res) => {
+  res.send("ignite visibility backend server is ready.");
+});
+
+app.listen(port, () => {
+  console.log("Listening from port", port);
+});
